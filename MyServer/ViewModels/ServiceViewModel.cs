6ce383e2614d36d4dfad7eb7180d0733cee0ca55
yourickds <youricks1@gmail.com>
2025-04-07 17:40:00 +0700

    Service MVVM, .gitignore
    Сделал viewModel для списка служб и привязал ListBox к Store для получения всех служб из бд. А так-же добавил в .gitignore файлы бд

---
+++ MyServer/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MyServer.Models;
+using MyServer.Stores;
+
+namespace MyServer.ViewModels
+{
+    class ServiceViewModel: INotifyPropertyChanged
+    {
+        private readonly ServiceStore _store;
+
+        private ObservableCollection<Service> _services = [];
+
+        public ServiceViewModel()
+        {
+            _store = new ServiceStore();
+            Services = _store.GetAllServices();
+        }
+
+        public ObservableCollection<Service> Services
+        {
+            get => _services;
+            set
+            {
+                _services = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
