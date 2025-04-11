839db41824c5dc7577b88e293111dfe0be5df8c4
yourickds <youricks1@gmail.com>
2025-04-11 18:36:06 +0700

    fix if
    Убрал проверку на null pid при запуске службы если процесс не был найден

--- MyServer/Actions/GetStartService.cs
+++ MyServer/Actions/GetStartService.cs
@@ -10,27 +10,24 @@ namespace MyServer.Actions
     {
         public static void Invoke(Service service)
         {
-            if (service.Pid == null)
+            Process process = new();
+            process.StartInfo.FileName = service.FilePath.Replace("%myserverdir%", AppDomain.CurrentDomain.BaseDirectory);
+            process.StartInfo.UseShellExecute = true;
+            if (!String.IsNullOrEmpty(service.Arguments))
             {
-                Process process = new();
-                process.StartInfo.FileName = service.FilePath.Replace("%myserverdir%", AppDomain.CurrentDomain.BaseDirectory);
-                process.StartInfo.UseShellExecute = true;
-                if (!String.IsNullOrEmpty(service.Arguments))
-                {
-                    process.StartInfo.Arguments = service.Arguments.Replace("%myserverdir%", AppDomain.CurrentDomain.BaseDirectory);
-                }
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                if (process.Start())
-                {
-                    service.Status = true;
-                    service.Pid = process.Id;
-                    ServiceStore.Instance.UpdateService(service);
-                    MessageBox.Show("Service Started. Pid: " + process.Id);
-                }
-                else
-                {
-                    MessageBox.Show("Не удалось запустить процесс");
-                }
+                process.StartInfo.Arguments = service.Arguments.Replace("%myserverdir%", AppDomain.CurrentDomain.BaseDirectory);
+            }
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            if (process.Start())
+            {
+                service.Status = true;
+                service.Pid = process.Id;
+                ServiceStore.Instance.UpdateService(service);
+                MessageBox.Show("Service Started. Pid: " + process.Id);
+            }
+            else
+            {
+                MessageBox.Show("Не удалось запустить процесс");
             }
         }
     }
