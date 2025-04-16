0e9a26b01d8d26d51d8a0af662cf6518100e2ea9
yourickds <youricks1@gmail.com>
2025-04-16 18:04:26 +0700

    StatusBar for Service
    Сделал верстку где выводим статус запуска службы и его идентификатор

--- MyServer/ViewModels/UpdateServiceViewModel.cs
+++ MyServer/ViewModels/UpdateServiceViewModel.cs
@@ -1,10 +1,26 @@
-﻿using MyServer.Models;
+﻿using MyServer.Actions;
+using MyServer.Models;
 
 namespace MyServer.ViewModels
 {
     class UpdateServiceViewModel: ServiceViewModelBase
     {
+        private bool _status;
+        private int? _pid;
+
         public Service SelectedService { get; }
+
+        public int? Pid
+        {
+            get { return _pid; }
+            set { _pid = value; OnPropertyChanged(); }
+        }
+
+        public bool Status
+        {
+            get { return _status; }
+            set { _status = value; OnPropertyChanged(); }
+        }
 
         public UpdateServiceViewModel(Service selectedService)
         {
@@ -13,6 +29,8 @@ namespace MyServer.ViewModels
             FilePath = SelectedService.FilePath.Replace("%myserverdir%\\", AppDomain.CurrentDomain.BaseDirectory);
             Arguments = SelectedService.Arguments?.Replace("%myserverdir%\\", AppDomain.CurrentDomain.BaseDirectory);
             Startup = SelectedService.Startup;
+            Pid = SelectedService.Pid;
+            Status = GetStatusService.Invoke(SelectedService);
         }
     }
 }
