337fdf883d706ed7c6b5eb0a6ddfc2f9851a672b
yourickds <youricks1@gmail.com>
2025-04-21 15:37:52 +0700

    TrayMenuIcon
    Установил пакет для работы с trayIcon. Подключил tray и добавил две базовые команды выход и настройки закрывающие приложение и показывающее окно если оно было закрыто. Теперь при закрытии окна приложение работает в фоне.

--- MyServer/MainWindow.xaml.cs
+++ MyServer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Navigation;
 
@@ -21,6 +22,17 @@ namespace MyServer
                 UseShellExecute = true
             });
             e.Handled = true;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // Отменяем стандартное закрытие окна
+            e.Cancel = true;
+
+            // Скрываем окно вместо закрытия
+            this.Hide();
+
+            base.OnClosing(e);
         }
     }
 }
---
+++ MyServer/Resources/TrayMenuResources.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using MyServer.Actions;
+using MyServer.Models;
+using MyServer.Stores;
+
+namespace MyServer.Resources
+{
+    public partial class TrayMenuResources: ResourceDictionary
+    {
+        private void ShowMainWindow(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow == null) return;
+
+            // Показываем окно
+            Application.Current.MainWindow.Show();
+
+            // Восстанавливаем состояние окна
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+
+            // Активируем окно (переводим фокус)
+            Application.Current.MainWindow.Activate();
+
+            // Если окно было свернуто - разворачиваем
+            if (Application.Current.MainWindow.WindowState == WindowState.Minimized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+
+            // Переводим окно на передний план
+            Application.Current.MainWindow.Topmost = true;
+            Application.Current.MainWindow.Topmost = false;
+        }
+
+        private void AppExit(object sender, RoutedEventArgs e)
+        {
+            // 1. Закрываем все окна приложения
+            foreach (Window window in Application.Current.Windows)
+            {
+                window.Close();
+            }
+
+            // 2. Останавливаем все запущенные службы
+            foreach (Service service in ServiceStore.Instance.Services)
+            {
+                GetStopService.Invoke(service);
+            }
+
+            // 3. Очищаем файл Hosts
+            ClearDomainHosts.Invoke();
+
+            // 4. Завершаем работу приложения
+            Application.Current.Shutdown();
+        }
+    }
+}
